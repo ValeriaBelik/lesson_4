41078cb5639d4271cc7645369127017621d3ae4e
Valeria <116289971+ValeriaBelik@users.noreply.github.com>
2022-11-03 17:22:07 +0200

    Add files via upload

---
+++ task_4.py
@@ -0,0 +1,34 @@
+object = 2000
+
+if object == 2000>1900 and 2000<1000000:
+    print("satisfied, check the year")
+else:
+    print("not satisfied, finish the program")
+
+object = 2000
+if object == 2000%100:
+    print("is not a leap year")
+else:
+    print("a leap year")
+
+
+object = 2400
+
+if object == 2400>1900 and 2400<1000000:
+    print("satisfied, check the year")
+else:
+    print("not satisfied, finish the program")
+
+object = 2400
+if object == 2400%100:
+    print("is not a leap year")
+else:
+    print("a leap year")
+
+
+
+object = 1800
+if object == 1800>1900 and 1800<1000000:
+    print("satisfied, check the year")
+else:
+    print("not satisfied, finish the program")
